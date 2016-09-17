6e0e23795120caa4d0eb8863e994faf112a30b80
Arkadiusz Kaczorowski <--->
2016-09-17 12:22:32 +0200

    Added branded start window example. Part1

---
+++ BrandedStartWindow/app/src/main/java/akaczorowski/pl/android/research/brandedstartwindow/MainActivity.java
@@ -0,0 +1,14 @@
+package akaczorowski.pl.android.research.brandedstartwindow;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    setTheme(R.style.AppTheme);
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
