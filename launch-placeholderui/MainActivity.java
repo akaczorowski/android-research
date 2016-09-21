52569ec53f78d4c7de9c1f8f265dbb0cdc5b106b
Arkadiusz Kaczorowski <--->
2016-09-21 21:57:22 +0200

    Added placeholder ui launch pattern.

---
+++ launch-placeholderui/MainActivity.java
@@ -0,0 +1,19 @@
+package research.android.pl.akaczorowski.placeholderui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    setTheme(R.style.AppTheme);
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+    setSupportActionBar(toolbar);
+    getSupportActionBar().setElevation(8);
+  }
+}
