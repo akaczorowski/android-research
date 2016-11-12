dfd2892502ef311d24df9f74b8f0299541ae825c
Arkadiusz Kaczorowski <--->
2016-11-12 10:43:16 +0100

    Added improvments to placeholderui2.

--- placeholderui2/app/src/main/java/akaczorowski/pl/android/research/placeholderui2/MainActivity.java
+++ placeholderui2/app/src/main/java/akaczorowski/pl/android/research/placeholderui2/MainActivity.java
@@ -1,6 +1,5 @@
 package akaczorowski.pl.android.research.placeholderui2;
 
-import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
@@ -10,7 +9,11 @@ public class MainActivity extends AppCompatActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     ThemeUtils.ensureRuntimeTheme(this);
-//    SystemClock.sleep(2000l);
+    /**
+     * Delete this line - only for debug purpose
+     */
+    SystemClock.sleep(2000l);
+
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
