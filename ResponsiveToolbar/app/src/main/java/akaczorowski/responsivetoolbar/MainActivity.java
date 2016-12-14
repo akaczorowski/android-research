8fdb013fbd7945cd7965523edbed88c890e51057
Arkadiusz Kaczorowski <--->
2016-12-14 19:29:56 +0100

    [WIP] Reponsive toolbar.

--- ResponsiveToolbar/app/src/main/java/akaczorowski/responsivetoolbar/MainActivity.java
+++ ResponsiveToolbar/app/src/main/java/akaczorowski/responsivetoolbar/MainActivity.java
@@ -1,7 +1,13 @@
 package akaczorowski.responsivetoolbar;
 
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,29 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+    setSupportActionBar(toolbar);
+    getSupportActionBar().setDisplayShowHomeEnabled(true);
+    getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    toolbar.setNavigationIcon(ContextCompat.getDrawable(this, android.R.drawable.ic_dialog_email));
+    toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Toast.makeText(getApplicationContext(), "Clicked!", Toast.LENGTH_SHORT);
+        //What to do on back clicked
+      }
+    });
+  }
+
+
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    MenuInflater inflater = getMenuInflater();
+    inflater.inflate(R.menu.menu, menu);
+    return true;
   }
 }
