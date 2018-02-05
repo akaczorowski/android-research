ed71d898ae85a3ebddfff9f4aee048ba44204345
Arkadiusz Kaczorowski <--->
2018-02-05 15:32:49 +0100

    Added comments.

--- launch-branded/MainActivity.java
+++ launch-branded/MainActivity.java
@@ -7,7 +7,7 @@ public class MainActivity extends AppCompatActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
-    setTheme(R.style.AppTheme);
+    setTheme(R.style.AppTheme);  //in AndroidManifest.xml put in <application> theme, BrandedAppTheme
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
   }
